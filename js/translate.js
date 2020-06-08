fa2b8301943d4a87ceccbb3865666b435f38730c
mifferol <marishaaa20027@gmail.com>
2020-06-08 17:30:56 +0300

    Add files via upload

---
+++ js/translate.js
@@ -0,0 +1,137 @@
+var arrLang = {
+  ua: {
+    offer: "послуги",
+    about: "про нас",
+    advantages: "переваги",
+    trans: "переклади",
+    corporate: "корпоративні заняття",
+    student: "записатися на заняття",
+    contacts: "контакти",
+    courseDescr:
+      " Мовні курси , що спеціалізуються на викладанні англійської мови для всіх рівнів і вікових категорій.",
+    contactBtn: "зв'язатися з нами",
+    offerHeading: "Що ми пропонуємо?",
+    general: "Базова англійська",
+    beginner:
+      "Рівні Beginner-Advanced – вивчення мови з включенням всіх аспектів в компактних групах до 6 чоловік та індивідуально, в тому числі для дітей.",
+    exam: "Підготовка до екзаменів",
+    prep: "Підготовка до здачі ЗНО, IELTS, TOEFL, FCE та ін.",
+    spec: "Спеціалізована англійська",
+    spheres:
+      "Англійська для таких сфер як : ІТ, юриспруденція, фінанси, маркетинг, банкова справа, медицина та ін.",
+    online: "Онлайн заняття",
+    comfort:
+      "Комфортне вивчення англійської мови вдома чи на роботі за допомогою Skype. Індивідуальні заняття за різними програмами.",
+    individual: "Індивідуальні та групові заняття",
+    choice:
+      "Обирайте між заняттями в компактних групах чи індивідуальним графіком у зручний для вас час.",
+    more: "Ми - не тільки англійська",
+    langs:
+      "Є можливість вивчення німецької, польської та чеської мов. А також викладання української та російської для іноземців.",
+    aboutHeading: "Про нас",
+    aboutText:
+      " LANGUAGE CENTRE – команда молодих та креативних викладачів з багаторічним досвідом викладання англійської мови. Наш дружній колектив почав свою діяльність в 2011 році, і з тих пір провів тисячі занять із сотнями задоволених студентів. Ми пропонуємо лише сучасні методики викладання та персональний підхід до кожного учня. Наші двері відчинені для кожного, хто бажає вивчити мову або покращити наявний рівень знань.",
+    advantagesHeading: "Наші переваги",
+    innov: "Використання новітніх підходів та методик у вивченні мови.",
+    flex: "Гнучкий графік, широкий спектр послуг.",
+    period:
+      "Гарантоване оволодіння мовою на певному рівні за короткий час (від 3 до 5 місяців).",
+    friendly: "Привітні та мотивовані молоді викладачі та чудова атмосфера.",
+    transHeading: "Переклади",
+    transText:
+      "Професійні переклади з англійської мови на українську та російську та навпаки на будь-яку тематику, зокрема :",
+    trans1: "Переклади загальної та специфічної тематики.",
+    trans2: "Технічні переклади.",
+    trans3: "Літературні переклади.",
+    trans4: "Переклади відео- та аудіозаписів.",
+    trans5: "Переклад документів.",
+    corporateHeading: "Корпоративні заняття",
+    corporText:
+      " Корпоративні заняття - чудова можливість для співробітників вашої компанії покращити рівень англійської мови. Ми пропонуємо базову англійську для всіх рівнів, бізнес курс, спеціалізовані курси для різноманітних сфер, а також підготовку до технічних інтерв'ю та комплексне тестування для співробітників та кандидатів на роботу.",
+    start: "Готові почати?",
+    startText:
+      "Для того, щоб почати навчання в Language Centre, потрібно заповнити анкету, пройти тест для визначення рівня володіння англійською мовою,визначитися зі зручним часом та формою навчання та чекати дзвінка!",
+    step1: "ЗАПОВНІТЬ АНКЕТУ",
+    step2: "ПРОЙДІТЬ ТЕСТ",
+    step3: "ОБЕРІТЬ ЧАС",
+    step4: "ЧЕКАЙТЕ ДЗВІНКА",
+    contactsHeading: "Контакти",
+    ad: "Адреса",
+    tel: "Телефон",
+    mob: "Мобільний",
+    em: "E-mail",
+    address: "58022, м. Чернівці, вул. Герцена, 1а, 3-й поверх, к.41-42 ",
+  },
+  en: {
+    offer: "what we offer",
+    about: "about us",
+    advantages: "advantages",
+    trans: "translations",
+    corporate: "corporate lessons",
+    student: "become a student",
+    contacts: "contact us",
+    courseDescr:
+      "Language school specializing in teaching English to all levels and age groups.",
+    contactBtn: "contact us",
+    offerHeading: "What do we offer",
+    general: "General English",
+    beginner:
+      "Beginner through Advanced levels. All aspects included; practicing English in small groups or individually",
+    exam: "Exam prep",
+    prep:
+      "Preparation for internal and international exams (IELTS, TOEFL, FCE, CAE etc)",
+    spec: "English for Specific Purposes",
+    spheres:
+      "English practice for different spheres including IT, Law, Finance, Marketing, Banking, Medical English and many more",
+    online: "Online sessions",
+    comfort:
+      "Learn English in the comfort of your own home or at work using Skype! (Viber or Zoom).Individual approach, tailor-made materials",
+    individual: "Individual and group lessons",
+    choice:
+      "The choice is yours – compact groups or individual sessions at you own pace",
+    more: "English and more…",
+    langs:
+      "We offer German, Polish and Czech as well as Ukrainian and Russian for foreigners",
+    aboutHeading: "About us",
+    aboutText:
+      " LANGUAGE CENTRE – a team of young and creative teachers with multiple years of experience in teaching English. We started out in 2011, and since then we have conducted thousands of lessons with hundreds of satisfied students. We offer only modern methods of teaching and personal approach towards every student. Our doors are always open for everyone who wants to learn English from scratch or improve their knowledge. ",
+    advantagesHeading: "Our advantages",
+    innov: "Innovative teaching methods and techniques.",
+    flex: "Flexible schedule, broad range of services",
+    period:
+      "Language acquisition at a certain level in a short period of time (3 to 5 months)",
+    friendly: "Friendly and motivated teachers, engaging lessons",
+    transHeading: "Translations",
+    transText:
+      "We offer professional translations from English, Ukrainian and Russian languages:",
+    trans1: "General and specialized translation",
+    trans2: "Technical translation",
+    trans3: "Literary translation",
+    trans4: "Translations of audio and video files",
+    trans5: "Documents translation",
+    corporateHeading: "Corporate sessions",
+    corporText:
+      " We offer your company a great opportunity to improve the English level of your employees. You can choose from a variety of courses including basic English, Business English course, ESP courses as well as job/technical interview preparation and complex evaluation for your employees and prospective candidates.",
+    start: "Become a student",
+    startText:
+      "To enroll, fill in the application form, take the evaluation test, choose your preferable hours and types of lessons and wait for our call!",
+    step1: "FILL IN THE APPLICATION FORM",
+    step2: "TAKE THE TEST",
+    step3: "CHOOSE YOUR SCHEDULE",
+    step4: "WAIT FOR THE CALL",
+    contactsHeading: "Contact us",
+    ad: "Address",
+    tel: "Office phone number",
+    mob: "Mobile",
+    em: "E-mail",
+    add: "58022, Chernivtsi, 1A Hertsena St., 3rd floor, office 41-42",
+  },
+};
+$(function () {
+  $(".translate").click(function () {
+    var lang = $(this).attr("id");
+    $(".lang").each(function (index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+  });
+});
